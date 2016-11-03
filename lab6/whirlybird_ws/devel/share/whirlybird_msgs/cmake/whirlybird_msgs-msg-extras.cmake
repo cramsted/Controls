9add956282845839813b80634e3606e1ad24c53d
Clayton Ramstedt <clayton.ramstedt@gmail.com>
2016-11-03 15:26:06 -0600

    added the whirlybird workspace with our code for lab6

---
+++ lab6/whirlybird_ws/devel/share/whirlybird_msgs/cmake/whirlybird_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(whirlybird_msgs_MESSAGE_FILES "/home/mark/Dropbox/controlbook/whirlybird/pythonCode/whirlybird_ws/src/whirlybird_msgs/msg/Whirlybird.msg;/home/mark/Dropbox/controlbook/whirlybird/pythonCode/whirlybird_ws/src/whirlybird_msgs/msg/Command.msg")
+set(whirlybird_msgs_SERVICE_FILES "")
